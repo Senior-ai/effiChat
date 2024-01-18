4024c79a99d6512d4ef79ee8586c143db5d5705a
Shon Aizenshtein <68730456+Senior-ai@users.noreply.github.com>
2024-01-18 16:08:49 +0200

    Added auth

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import trimRequest from 'trim-request';
+import { register, login, logout, refreshToken } from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.route('/register').post((req, res) => {
-    res.send('Register API')
-});
+router.route('/register').post(trimRequest.all,register);
+router.route('/login').post(trimRequest.all,login);
+router.route('/logout').post(trimRequest.all,logout);
+router.route('/refreshToken').post(trimRequest.all,refreshToken);
 export default router;
