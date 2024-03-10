4f6a0d82bb4ee7f2630f251f163bf9f622482a62
Shon Aizenshtein <68730456+Senior-ai@users.noreply.github.com>
2024-03-10 18:00:37 +0200

    debug

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -1,64 +1,63 @@
-import logger from './configs/logger.js';
+import logger from "./configs/logger.js";
 //not an actual server but you get the point. Maybe I should rename it SocketService instead
 let onlineUsers = [];
 export default function (socket, io) {
-    //user joins or opens the conversation
-    socket.on('join', (user) => {
-        logger.info('User joined', user);
-        socket.join(user);
-        if (!onlineUsers.some((u) => u.userId === user)) {
-          onlineUsers.push({userId: user, socketId: socket.id});
-        }
-        io.emit('getOnlineUsers', onlineUsers);
-        // Send socket id
-        io.emit('setupSocket', socket.id);
+  //user joins or opens the conversation
+  socket.on("join", (user) => {
+    logger.info("User joined", user);
+    socket.join(user);
+    if (!onlineUsers.some((u) => u.userId === user)) {
+      onlineUsers.push({ userId: user, socketId: socket.id });
+    }
+    io.emit("getOnlineUsers", onlineUsers);
+    // Send socket id
+    io.emit("setupSocket", socket.id);
+  });
+
+  //socket disconnect
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+
+  //Joins a conversation room
+  socket.on("join conversation", (conversation) => {
+    socket.join(conversation);
+  });
+
+  //Send and receive message
+  socket.on("send message", (message) => {
+    let conversation = message?.conversation;
+    console.log(message);
+    if (!conversation.users) return;
+    conversation.users.forEach((user) => {
+      if (user._id === message.sender._id) return;
+      socket.in(user._id).emit("message received", message);
     });
+  });
 
-    //socket disconnect
-    socket.on('disconnect', () => {
-        onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
-        io.emit('getOnlineUsers', onlineUsers);
-    })
+  socket.on("typing", (conversation) => {
+    socket.in(conversation).emit("typing", conversation);
+  });
 
-    //Joins a conversation room
-    socket.on('join conversation', (conversation) => {
-        socket.join(conversation);
+  socket.on("stop typing", (conversation) => {
+    socket.in(conversation).emit("stop typing", conversation);
+  });
 
-    })
-
-    //Send and receive message
-    socket.on('send message', (message) => {
-        let conversation = message?.conversation;
-        console.log(message)
-        if (!conversation.users) return;
-        conversation.users.forEach((user) => {
-            if (user._id === message.sender._id) return;
-            socket.in(user._id).emit('message received', message);
-        })
+  //Call
+  socket.on("callUser", (data) => {
+    let userId = data.userToCall;
+    let userSocketId = onlineUsers.find((user) => user.userId === userId);
+    io.to(userSocketId?.socketId).emit("callUser", {
+      signal: data.signal,
+      from: data.from,
+      name: data.name,
+      picture: data.picture,
     });
+  });
 
-    socket.on('typing', (conversation) => {
-        socket.in(conversation).emit('typing', conversation);
-    })
-    
-    socket.on('stop typing', (conversation) => {
-        socket.in(conversation).emit('stop typing', conversation);
-    })
-
-    //Call
-    socket.on('callUser', (data) => {
-        let userId = data.userToCall;
-        let userSocketId = onlineUsers.find((user) => user.userId === userId);
-        io.to(userSocketId.socketId).emit('callUser', {
-            signal: data.signal,
-            from: data.from,
-            name: data.name,
-            picture: data.picture
-        });
-    })
-
-    //Answer call
-    socket.on('answerCall', (data) => {
-        io.to(data.to).emit('callAccepted', data.signal);
-     })
+  //Answer call
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
 }
